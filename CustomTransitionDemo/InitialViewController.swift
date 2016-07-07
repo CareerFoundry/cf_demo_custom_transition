233b88605bbf06db13781ee5499d989f037b7ccf
jimbroski <sam.bogu@gmail.com>
2016-07-07 16:59:35 +0200

    initial commit

---
+++ CustomTransitionDemo/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CustomTransitionDemo
+//
+//  Created by Hesham Abd-Elmegid on 6/19/16.
+//  Copyright © 2016 CareerFoundy. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
